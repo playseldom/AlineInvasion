2d6fd86d87d15bbdecf73730f63eb79d7211251c
playseldom <2458702981@qq.com>
2022-07-21 20:11:28 +0800

    增加了子弹这一物品

--- aline_invasion.py
+++ aline_invasion.py
@@ -38,7 +38,7 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
-            elif event.type == pygame.KEYDOWN:
+            elif event.type == pygame.KEYDOWN
                 self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
